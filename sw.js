0cfc7b58ac02c592f5ae94786bbd875ce5f4c0bb
purplesite <armyinsta80@gmail.com>
2026-08-21 19:30:02 -0600

    Update service worker to version 9

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER - RULETA PWA
 // ==========================================
 
-const CACHE_NAME = "ruleta-pwa-v8";
+const CACHE_NAME = "ruleta-pwa-v9";
 
 const ARCHIVOS = [
     "./",
